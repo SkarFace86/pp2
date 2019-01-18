183a82b863772e26bbc01aead04e438499c622a4
Brooks Eshe <beshe@mail.greenriver.edu>
2019-01-18 15:15:02 -0500

    routed forms

--- index.php
+++ index.php
@@ -34,13 +34,17 @@ $f3->route('GET /order', function() {
 });
 
 $f3->route('POST /order2', function() {
+    $_SESSION['color'] = $_POST['color'];
     $view = new View();
     echo $view->render('views/form2.html');
 });
 
 $f3->route('POST /results', function() {
-    $view = new View();
-    echo $view->render('views/results.html');
+    //$color = $_SESSION['color'];
+    //$animal = $_SESSION['animal'];
+    //echo "<h3>Thank you for ordering a $color $animal</h3>";
+    $template = new Template();
+    echo $template->render('views/results.html');
 });
 
 //Route for animal type
