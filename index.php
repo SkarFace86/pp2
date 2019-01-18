448b7245d0396e2acc6421081fdfb9a072129206
Brandon Skar <bransk01@yahoo.com>
2019-01-18 15:00:20 -0500

    Added results page, updated routes index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
  * Initiate fat free
  */
 
+session_start();
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -29,6 +30,16 @@ $f3->route('GET /', function() {
 $f3->route('GET /order', function() {
     $view = new View();
     echo $view->render('views/form1.html');
+});
+
+$f3->route('POST /order2', function() {
+    $view = new View();
+    echo $view->render('views/form2.html');
+});
+
+$f3->route('POST /results', function() {
+    $view = new View();
+    echo $view->render('views/results.html');
 });
 
 //Route for animal type
