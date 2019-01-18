bea51f3aa5020d7623852187ea12d38151b40fb9
Brandon Skar <bransk01@yahoo.com>
2019-01-18 15:28:40 -0500

    updated forms and completed index.php, results page displays final order

--- index.php
+++ index.php
@@ -28,20 +28,20 @@ $f3->route('GET /', function() {
 });
 
 $f3->route('GET /order', function() {
-    $_SESSION['animal'] = $_POST['animal'];
     $view = new View();
     echo $view->render('views/form1.html');
 });
 
 $f3->route('POST /order2', function() {
-    $_SESSION['color'] = $_POST['color'];
+    $_SESSION['animal'] = $_POST['animal'];
     $view = new View();
     echo $view->render('views/form2.html');
 });
 
 $f3->route('POST /results', function() {
-    //$color = $_SESSION['color'];
-    //$animal = $_SESSION['animal'];
+    $_SESSION['color'] = $_POST['color'];
+    $color = $_SESSION['color'];
+    $animal = $_SESSION['animal'];
     //echo "<h3>Thank you for ordering a $color $animal</h3>";
     $template = new Template();
     echo $template->render('views/results.html');
